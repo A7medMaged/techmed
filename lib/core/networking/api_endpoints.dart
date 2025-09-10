c3add0bd8188549e9d5e6b321aa55f49f91ab4ad
AhmedMaged1722 <am9351705@gmail.com>
2025-09-10 18:21:17 +0300

    - Implement API endpoints and enhance DioHelper for network requests

--- lib/core/networking/api_endpoints.dart
+++ lib/core/networking/api_endpoints.dart
@@ -1,3 +1,7 @@
 class ApiEndpoints {
+  static const String baseUrl = 'https://techmed-api.vercel.app/api/v1';
 
+  static const String login = '$baseUrl/auth/login';
+
+  static const String register = '$baseUrl/auth/register';
 }
--- lib/core/networking/dio_helper.dart
+++ lib/core/networking/dio_helper.dart
@@ -1,6 +1,48 @@
 import 'package:dio/dio.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+import 'package:techmed/core/errors/failure.dart';
+import 'package:techmed/core/networking/api_endpoints.dart';
 
 class DioHelper {
-  final Dio dio;
-  DioHelper._internal(this.dio);
+  Dio? dio;
+
+  DioHelper() {
+    dio ??= Dio(
+      BaseOptions(
+        baseUrl: ApiEndpoints.baseUrl,
+        receiveDataWhenStatusError: true,
+      ),
+    );
+
+    dio!.interceptors.add(PrettyDioLogger());
+  }
+
+  Future<Response> getRequest({
+    required String endPoint,
+    Map<String, dynamic>? query,
+  }) async {
+    try {
+      final Response response = await dio!.get(
+        endPoint,
+        queryParameters: query,
+      );
+
+      return response;
+    } on DioException catch (e) {
+      throw ServerFailure.fromDioError(e);
+    }
+  }
+
+  Future<Response> postRequest({
+    required String endPoint,
+    Map<String, dynamic>? data,
+  }) async {
+    try {
+      final Response response = await dio!.post(endPoint, data: data);
+
+      return response;
+    } on DioException catch (e) {
+      throw ServerFailure.fromDioError(e);
+    }
+  }
 }
