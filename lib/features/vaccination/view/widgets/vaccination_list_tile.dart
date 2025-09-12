377481a2d5b286bde8d870f6b62c07ddad102960
AhmedMaged1722 <am9351705@gmail.com>
2025-09-12 04:36:51 +0300

    - Refactor VaccinationListTile to improve navigation handling and integrate VaccinationCubit for state management

--- lib/features/vaccination/view/widgets/vaccination_list_tile.dart
+++ lib/features/vaccination/view/widgets/vaccination_list_tile.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:techmed/core/routing/app_routes.dart';
 import 'package:techmed/core/styling/app_styles.dart';
 import 'package:techmed/features/vaccination/data/models/vaccination_model/datum.dart';
+import 'package:techmed/features/vaccination/logic/cubit/vaccination_cubit.dart';
 
 class VaccinationListTile extends StatelessWidget {
   const VaccinationListTile({super.key, required this.vaccination});
@@ -25,10 +27,17 @@ class VaccinationListTile extends StatelessWidget {
       trailing: const Icon(Icons.arrow_forward_ios),
       onTap: () {
         if (vaccination.id != null) {
-          context.push(
-            AppRoutes.vaccinationDetailsScreen,
-            extra: vaccination.id!,
-          );
+          context
+              .push(
+                AppRoutes.vaccinationDetailsScreen,
+                extra: vaccination.id!,
+              )
+              .then((result) {
+                if (result == true) {
+                  // ignore: use_build_context_synchronously
+                  context.read<VaccinationCubit>().getVaccinations();
+                }
+              });
         }
       },
     );
